7c216e09ad24d7ece887fd9d2e665dbf997bfe03
Lecislav <lecislav@protonmail.com>
2023-05-07 18:08:29 +0200

    ver 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Key-Reader-Logger
+ENABLE ONLY ON WINDOWS
+Package provide utility to capture input data from keyboard and mouse. Consist of two separete process wrote in cpp(source file included).Those process create windows hooks to capture data;
+
+Work of  class KeyReaderLogger base on two events: "keyData" and "mouseData";
+
+kLog.on('mouseData',(data)=>{
+  console.log(data);
+});
+
+kLog.on("keyData", (data) => {
+  console.log(data);
+});
+
+Remember to use function start() to initialize subprocess.
+See example in example.js.
--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 // import KeyReaderLogger from "./KeyReaderLogger.js";
-import  {KeyReaderLogger} from "./KeyReaderLogger.js";
+import  {KeyReaderLogger} from "key-reader-logger";
 
 const k = new KeyReaderLogger();
 const kLog = new KeyReaderLogger();
@@ -10,46 +10,3 @@ kLog.on('mouseData',(data)=>{
 kLog.on("keyData", (data) => {
   console.log(data);
 });
-// console.log(kLog);
-//     kLog.childProcess.on("data", (data) => {
-//       console.log(`stdout: ${data}`);
-//     });
-
-
-
-// console.log("lol");
-// const lol = require("./build/Release/my_addon.node");
-// console.log(lol.pass_number());
-// let i=0;
-// fs.watch("myfile.txt", (eventType, filename) => {
-//   fs.stat('myfile.txt',(err,stats)=>{
-//     if(stats.size>0){
-//       console.log(`event type is: ${eventType}`);
-//       i++;
-//       console.log(i);
-//       fs.readFile("myfile.txt",'utf8',(err,data)=>{
-//         console.log(data);
-//       });
-//       fs.writeFile("myfile.txt", "",err=>{
-//         console.log(err);
-//       });
-//     }
-//   })
-
-// });
-// const user32 = ffi.Library("user32", {
-//   MessageBoxA: ["int", ["int", "string", "string", "int"]],
-// });
-
-// const MB_OKCANCEL = 1;
-// const IDOK = 1;
-// const IDCANCEL = 2;
-
-// const ret = user32.MessageBoxA(null, "Hello World!", "Node.js", MB_OKCANCEL);
-// if (ret === IDOK) {
-//   console.log("User clicked OK");
-// } else if (ret === IDCANCEL) {
-//   console.log("User clicked Cancel");
-// } else {
-//   console.log("User closed the message box");
-// }
